e32fd970b38fb12e2c5c25e049e9deae9576ba83
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-24 17:13:44 +0700

    b2-cloudClient:  start project, add file properties

---
+++ cloud-config-client-company/src/main/java/com/example/cloudconfigclientcompany/CloudConfigClientCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfigclientcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigClientCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigClientCompanyApplication.class, args);
+	}
+
+}
---
+++ cloud-config-client-company/src/test/java/com/example/cloudconfigclientcompany/CloudConfigClientCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfigclientcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigClientCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
