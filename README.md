096fcbaed0c53f00a317366187681f47ed244a0e
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-24 07:59:52 +0700

    bai2: Clould config server ERR

---
+++ README.md
@@ -0,0 +1,1 @@
+# lab08_APIgetway-cloudConfig-Curcuit
---
+++ demo-spring-clould-config2/src/test/java/spring/cloud/config/demospringclouldconfig/DemoSpringClouldConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.config.demospringclouldconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringClouldConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
